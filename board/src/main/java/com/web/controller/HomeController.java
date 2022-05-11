d8527582955783bb759cbbbdeba220e7a5f68a52
kwonseeun <0115km@naver.com>
2022-05-11 14:26:42 +0900

    register.jsp , /register Controller 추가

--- board/src/main/java/com/web/controller/HomeController.java
+++ board/src/main/java/com/web/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.web.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.web.dto.BoardDTO;
 import com.web.service.BoardService;
 import com.web.service.UserService;
 
@@ -22,5 +25,18 @@ public class HomeController {
 		@RequestMapping("/")
 		public String home(Model model) {
 				return "home";
+		}
+		
+		@RequestMapping("/register")
+		public String register(Model model) {
+			return "register";
+		}
+		
+		@RequestMapping("/select")
+		public String select(Model model) {
+			List<BoardDTO> boardList = bService.getBoardList();
+			model.addAttribute("boardList", boardList);
+			
+			return "select";
 		}
 }
